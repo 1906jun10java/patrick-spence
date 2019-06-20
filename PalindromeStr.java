20e9b0718dd778e3667b9f1d2bf32eb65ab031b9
patrick-spence <patrickspence27@gmail.com>
2019-06-19 22:20:36 -0400

    palindrome complete with new code. using the substring method

--- PalindromeStr.java
+++ PalindromeStr.java
@@ -3,39 +3,54 @@ import java.util.Scanner;
 public class PalindromeStr {
 
 	public static void main(String[] args) {
-		Scanner phrase = new Scanner(System.in); //creates a scanner object 
-		System.out.println("Enter a Palindrome: "); 
-		String palindrome = phrase.nextLine(); //user enters the palindrome 
-		System.out.println("You've entered: "+palindrome);
+		Scanner phrase = new Scanner(System.in); // creates a scanner object
+		System.out.println("Enter a Palindrome: ");
+		String palindrome = phrase.nextLine(); // user enters the palindrome
+		System.out.println("You've entered: " +palindrome);
 
-		//int a = palindrome.length(); //defines the length of the palindrome 
-		//System.out.println(a);
+		// int a = palindrome.length(); //defines the length of the palindrome
+		// System.out.println(a);
+		phrase.close();
 
-
-		System.out.println(reverse(palindrome));
+		reverse(palindrome);
+		//PalindromeStr.Boolean(palindrome);
 	}
+
 
 	public static String reverse(String palindrome) {
-		String input = palindrome; //palindrome input 
-		String output = " "; 
+		// String input = " "; //palindrome input
+		String output = " ";
+		for (int p = palindrome.length() - 1; p >= 0; p--) {
+			output = output + palindrome.charAt(p);
+		}
 	
-		int a = 7;
-		if(palindrome.length() == a) { //if the palindrome length is equal to 7
-			for(int i = palindrome.length() - 1; i >= 0; i--) {//to write the palindrome in reverse 
-				output = output + palindrome.charAt(i); 
-			}
-			System.out.println(output+", which is "+palindrome+" reversed. A palindrome.");	
-			/*
-		} else if (palindrome.charAt(i) = output) {//if the entered input isn't a palindrome 
-			System.out.println("Enter a new Palindrome: ");
-			Scanner phrase2 = new Scanner(System.in);
-			String palindrome2 = phrase2.nextLine();t
-			System.out.println("You've Entered: "+palindrome2);
-			*/
+	
+
+	//public static void Boolean(String palindrome) {
+	if ((output.substring(0, 8)==(output.substring(0, 8)))) {  //used the substring method to construct if the word is a palindrome
+		/*
+		 * with the beginning index set at 0 and the ending set at 6, since 0 is the starting position and 8 spaces will equal 9
+		 * the output would run, equating the values of the string between those indexes 
+		 * granted there not below the index length
+		 */
+			System.out.println(output+", which is reversed. A palindrom.");
+		/*
+		 * only issue is, with words that are 7 integers long, the code will read it as a palindrome
+		 * which is incorrect
+		 */
 		}else {
-				System.out.println(palindrome+" is not a palindrome, and your program has been terminated!");
-			}
-		return " ";
+			System.out.println(output+" is not a palindrome, and your program has been terminated!");
+
+		}
+		return output;
 	}
+
 }
 
+
+/*
+ * } else if (palindrome.charAt(i) = output) {//if the entered input isn't a
+ * palindrome System.out.println("Enter a new Palindrome: "); Scanner phrase2 =
+ * new Scanner(System.in); String palindrome2 = phrase2.nextLine();t
+ * System.out.println("You've Entered: "+palindrome2);
+ */
